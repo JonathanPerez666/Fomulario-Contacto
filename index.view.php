9bf738ffbe5e172ae74f66bf17656f57f51e3d10
Jonathan <mcjhona069@gmail.com>
2020-06-25 20:06:26 -0500

    creando estilos CSS

--- index.view.php
+++ index.view.php
@@ -12,7 +12,7 @@
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
             <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre:" value="">
 
-            <input type="text" class="form-control" id="correo" name="correo" placeholder="Correo:" value="">
+            <input type="email" class="form-control" id="correo" name="correo" placeholder="Correo:" value="">
 
             <textarea name="mensaje" class="form-control" id="mensaje" placeholder="Mensaje:"></textarea>
 
